8be50d22393a0f4826525b4e448574a97f2292c0
burillu <burillu90@hotmail.it>
2023-10-18 18:43:11 +0200

    created two arrays with different legth

--- array_bonus/js/script.js
+++ array_bonus/js/script.js
@@ -1,11 +1,27 @@
 
 
-// creo un array con dei numeri casuali
+// creo due array con dei numeri casuali
 
-let randomic = [];
-let lengthArray= 5;
-// ciclo e ritorno ogni numero dentro randomic
+let randomic1 = [];
+let randomic2 = [];
+let lengthArray= 25;
+// ciclo e spingo ogni numero dentro randomic1
 for (let i = 0; i < lengthArray; i++) {
-    const element = array[i];
-    
+    const num = getRndInteger(1, 100);
+    randomic1.push(num);
+
+}
+// ciclo e spingo ogni numero dentro randomic2
+for (let i = 0; i < lengthArray+15; i++) {
+    const num = getRndInteger(1, 100);
+    randomic2.push(num);
+
+}
+
+console.log(`primo array : ${randomic1}`);
+console.log(`secondo array : ${randomic2}`);
+
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }

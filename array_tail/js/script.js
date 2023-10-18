ddc08214973937bafaedb4ab4f82feb91260062e
burillu <burillu90@hotmail.it>
2023-10-18 17:57:30 +0200

    add input, button, listener, function work

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -1,23 +1,33 @@
 
+// prendo input lunghezza array
+const inputNum = document.getElementById('input-num');
+//prendo input numeri da stampare
+const inputNumPrint = document.getElementById('input-num-print');
+//prendo bottone
+const btn = document.getElementById('btn-go');
+btn.addEventListener('click', createNumbers);
 
-// creo un array con dei numeri casuali
+function createNumbers() {
 
-let randomic = [];
-let lengthArray= 25;
-// ciclo e ritorno ogni numero dentro randomic
-for (let i = 0; i < lengthArray; i++) {
-    const num = getRndInteger(1,100);
-    randomic.push(num);
-    console.log(num);
-    
+    // creo un array con dei numeri casuali
+
+    let randomic = [];
+    let lengthArray = parseInt(inputNum.value);
+    // ciclo e spingo ogni numero dentro randomic
+    for (let i = 0; i < lengthArray; i++) {
+        const num = getRndInteger(1, 100);
+        randomic.push(num);
+
+    }
+    console.log(randomic);
+
+    // stampare ultimi 5 numeri
+
+    console.log(randomic.slice(-5))
 }
-console.log(randomic);
 
-// stampare ultimi 5 numeri
-
-console.log(randomic.slice(-5))
 
 
 function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-  }
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}

9daf3d3a4372887b0d155d53019501693c182951
burillu <burillu90@hotmail.it>
2023-10-18 17:04:44 +0200

    solve logic part

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -3,9 +3,21 @@
 // creo un array con dei numeri casuali
 
 let randomic = [];
-let lengthArray= 5;
+let lengthArray= 25;
 // ciclo e ritorno ogni numero dentro randomic
 for (let i = 0; i < lengthArray; i++) {
-    const element = array[i];
+    const num = getRndInteger(1,100);
+    randomic.push(num);
+    console.log(num);
     
 }
+console.log(randomic);
+
+// stampare ultimi 5 numeri
+
+console.log(randomic.slice(-5))
+
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }

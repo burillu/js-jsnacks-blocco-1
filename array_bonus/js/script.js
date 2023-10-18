278c6ae8b8e585fdba16251114872d4ffb6ac062
burillu <burillu90@hotmail.it>
2023-10-18 16:49:04 +0200

    add bonus dir

---
+++ array_bonus/js/script.js
@@ -0,0 +1,11 @@
+
+
+// creo un array con dei numeri casuali
+
+let randomic = [];
+let lengthArray= 5;
+// ciclo e ritorno ogni numero dentro randomic
+for (let i = 0; i < lengthArray; i++) {
+    const element = array[i];
+    
+}
